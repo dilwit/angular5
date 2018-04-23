c7d9bddf64163e3bd9a0752e0fbb71e710054362
Dilusha Withanage <dilusha.withanage@gmail.com>
2018-04-23 22:38:22 +1000

    oneway / two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -18,7 +19,8 @@ import { ToDoComponent } from './to-do/to-do.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoComponent implements OnInit {
 
+  // One way data-binding
+  placeHolderText: string = "Go to gym tomorrow...";
+  submitText: string = "Add Item";
+  toDoList: string = [];
+
+  // Two way data-binding
+  toDoItem: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem() {
+  	this.toDoList.push(this.toDoItem);
+  	this.toDoItem = null;
   }
 
 }
