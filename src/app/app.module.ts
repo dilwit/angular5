20390d61e82884cf5de178c87a841361193865e6
Dilusha Withanage <dilusha.withanage@gmail.com>
2018-04-23 21:59:28 +1000

    initial code check-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ToDoComponent } from './to-do/to-do.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactUsComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule,
